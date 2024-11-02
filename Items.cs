39bc3a89ca06e9be113d4fd1aa2cd7869d452183
Jonas <jonas.gustafsson85@gmail.com>
2024-11-02 10:59:00 +0100

    Gjort en struct med konstruktor för ett hjärta som ska ge spelaren liv

---
+++ Items.cs
@@ -0,0 +1,19 @@
+using System.Dynamic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework.Graphics;
+
+
+public struct Heart
+    {
+        public Vector2 Position {get; set;} 
+        public Texture2D Texture {get; set;}
+        public int Size{get; set;}
+
+        public Heart(Vector2 positioin, Texture2D texture, int size)
+        {
+            Position = Position;
+            Texture = texture;
+            Size = size;
+        }
+    }
