f1606fbe4a22741f97574a54c49ade6c9c269660
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2024-11-04 15:07:49 +0100

    En nästan tom commit för att testa

--- Items.cs
+++ Items.cs
@@ -10,15 +10,15 @@ public class Item
         public Vector2 Position {get;set;}
         public Texture2D Texture {get; set;}
         public int Size{get; set;}
-        public Random ItemRandom;
-        
+        private Random ItemRandom = new Random();
+                
 
-        public Heart(Vector2 position, Texture2D texture, int size, Random itemRandom)
+        public Heart(Vector2 position, Texture2D texture, int size)
         {
-            Position = new Vector2(itemRandom.Next(20, 1880), itemRandom.Next(20, 1040));
+            Position = new Vector2(ItemRandom.Next(20, 1880), ItemRandom.Next(20, 1040));
             Texture = texture;
             Size = size;
-            ItemRandom = itemRandom;
+            
         }
         public void SpawnHeart(SpriteBatch SpriteBatch, Color color)
         {
