9340b6b37fe27a4e0dbeee41dd2521eaa69d336d
Jonas <jonas.gustafsson85@gmail.com>
2024-11-04 11:41:00 +0100

    Fortsatt jobba med item och vårat hjärta

--- Items.cs
+++ Items.cs
@@ -1,20 +1,32 @@
-using System.Dynamic;
-using System.Numerics;
-using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Dynamic;
 
-
-public struct Heart
-   {
-        public Vector2 Position {get; set;} 
+public class Item
+{
+    public struct Heart
+    {
+        public Vector2 Position {get;set;}
         public Texture2D Texture {get; set;}
         public int Size{get; set;}
+        public Random ItemRandom;
+        
 
-        public Heart(Vector2 positioin, Texture2D texture, int size)
+        public Heart(Vector2 position, Texture2D texture, int size, Random itemRandom)
         {
-            Position = Position;
+            Position = new Vector2(itemRandom.Next(20, 1880), itemRandom.Next(20, 1040));
             Texture = texture;
             Size = size;
+            ItemRandom = itemRandom;
+        }
+        public void SpawnHeart(SpriteBatch SpriteBatch)
+        {
+            int ItemRandomX = ItemRandom.Next(20, 1880);
+            int ItemRandomY = ItemRandom.Next(20, 1040);
+            Position = new Vector2(ItemRandomX, ItemRandomY);
+            Rectangle heartRectangle = new Rectangle(ItemRandomX, ItemRandomY , Size, Size);
+            SpriteBatch.Draw(Texture, heartRectangle, Color.White);
         }
     }
-    
+}
